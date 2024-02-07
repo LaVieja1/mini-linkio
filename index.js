cc2309476438a9f40586cf31db0600ea224b4377
santicano <canosantiago404@gmail.com>
2024-02-07 14:11:59 -0300

    añadido index route

--- index.js
+++ index.js
@@ -15,11 +15,16 @@ const app = express();
 
 // ROUTES IMPORT
 const urlRoute = require("./routes/url");
+const indexRoute = require("./routes/index");
 
 //MIDDLEWARE
 app.use(express.json());
 
+// VIEW ENGINE
+app.set("view engine", "ejs");
+
 //ROUTE MIDDLEWARE
 app.use("/url", urlRoute);
+app.use("/", indexRoute);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
