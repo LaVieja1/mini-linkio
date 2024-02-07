3e38e908805a84b351f51042d1b7db5cb83b8df2
santicano <canosantiago404@gmail.com>
2024-02-07 16:54:09 -0300

    arreglado bug de dotenv y esperar a mongoDb para que inicie server

--- index.js
+++ index.js
@@ -3,23 +3,35 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.set("strictQuery", false);
-main().catch((err) => console.log(err));
 
-async function main() {
-  await mongoose.connect(process.env.MONGODB);
-  console.log("DB CONNECTED");
-}
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 const PORT = process.env.PORT;
 const app = express();
 
 // ROUTES IMPORT
 const urlRoute = require("./routes/url");
+const indexRoute = require("./routes/index");
 
 //MIDDLEWARE
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// VIEW ENGINE
+app.set("view engine", "ejs");
 
 //ROUTE MIDDLEWARE
 app.use("/url", urlRoute);
+app.use("/", indexRoute);
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+connectDB().then(() => {
+  app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+});
