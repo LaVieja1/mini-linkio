d2701e801907a3a923acd529a749aece990381db
santicano <canosantiago404@gmail.com>
2024-02-07 15:19:03 -0300

    añadido tabla funcional y modo oscuro

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index");
+const URL = require("../models/url");
+
+router.get("/", async (req, res) => {
+  const shortUrls = await URL.find();
+  res.render("index", { shortUrls: shortUrls });
 });
 
 module.exports = router;
