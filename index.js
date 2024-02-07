ca6ff7424e28625c7cd495ae1071438015d7e0dc
santicano <canosantiago404@gmail.com>
2024-02-07 12:52:56 -0300

    añadido middleware

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
-const app = express();
+mongoose.set("strictQuery", false);
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB);
+  console.log("DB CONNECTED");
+}
 
 const PORT = process.env.PORT;
+const app = express();
+
+// ROUTES IMPORT
+const urlRoute = require("./routes/url");
+
+//MIDDLEWARE
+app.use(express.json());
+
+//ROUTE MIDDLEWARE
+app.use("/url", urlRoute);
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
